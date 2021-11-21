834a7c9d50892051c8affd33b5dd4ae2c8183854
Francesco Generali <genemania@hotmail.it>
2021-11-21 17:02:36 +0100

    Minigame_1: Environment, Player and CPU

---
+++ Assets/Scripts/Minigame_Scripts/CPU_Behaviour.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CPU_Behaviour : MonoBehaviour
+{
+    public float speed;
+    public int numberOfPassengers;
+    public int passengersCapacity;
+    public TextMeshProUGUI passengersUI;
+
+    private Animator animator;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+    
+    void Start()
+    {
+        animator.speed = speed;
+
+        animator.SetBool("StartAnim", true);
+    }
+
+    void Update()
+    {
+        passengersUI.text = numberOfPassengers.ToString();
+    }
+
+    private void passengersGetInVehicle()
+    {
+        for (int i = 0; i < passengersCapacity; i++)
+        {
+            numberOfPassengers--;
+            if (numberOfPassengers == 0)
+                return;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("MG_Goal") && numberOfPassengers == 0)
+        {
+            Debug.Log("Fine!");
+        }
+        else if (other.CompareTag("MG_Start"))
+        {
+            passengersGetInVehicle();
+        }
+    }
+}
---
+++ Assets/Scripts/Minigame_Scripts/MG_Player_Movement.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MG_Player_Movement : MonoBehaviour
+{
+    public float speed;
+    public int numberOfPassengers;
+    public int passengersCapacity;
+    public TextMeshProUGUI passengersUI;
+
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        passengersUI.text = numberOfPassengers.ToString();
+    }
+
+    public void AddForce()
+    {
+        rb.AddForce(0, 0, speed * Time.deltaTime);
+    }
+
+    private void passengersGetInVehicle()
+    {
+        for (int i = 0; i < passengersCapacity; i++)
+        {
+            numberOfPassengers--;
+            if (numberOfPassengers == 0)
+                return;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("MG_Goal") && numberOfPassengers == 0)
+        {
+            Debug.Log("Fine!");
+        }
+        else if (other.CompareTag("MG_Start"))
+        {
+            passengersGetInVehicle();
+        }
+    }
+}
